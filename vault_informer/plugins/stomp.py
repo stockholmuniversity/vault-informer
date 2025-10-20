0d4dc1df9fa3739d4bebe7383b2d79412013b70b
Simon Lundström <simlu+github@su.se>
2025-10-20 17:47:52 +0200

    Use ConfigParser object for config
    And move config structure into a well defined structure. Using colon as
    separator seems to be best practice (at least according to our
    co-worker ; )

--- vault_informer/plugins/stomp.py
+++ vault_informer/plugins/stomp.py
@@ -6,7 +6,6 @@ import ssl
 import stomp
 from vault_informer.plugins import InformerPlugin
 
-PRODUCER_CONFIGFILE = "conf/consumer_conf.ini"
 CHECK_FILE_AGE_FILE = "/local/vault-informer/check_file_age_file"
 
 log = logging.getLogger(__name__)
@@ -27,36 +26,27 @@ def touch_file(filename):
         open(filename, "a").close()
 
 
-def load_config(self):
-    cfg = self.config
-
-    return {
-        "esb_host": cfg.get("esb", "hostname"),
-        "esb_user": cfg.get("esb", "username"),
-        "esb_password": cfg.get("esb", "password"),
-        "esb_queue": cfg.get("esb", "queue"),
-    }
-
-
 # pylint: disable=too-few-public-methods
 class Stomp(InformerPlugin):
     def __init__(self, config):
         self.config = config
 
     def handle_event(self, message):
-        config = load_config(self)
-        host = config["esb_host"]
+        host = self.config.get("plugins:stomp", "hostname")
         port = "61612"
-        username = config["esb_user"]
-        password = config["esb_password"]
-        queue_name = config["esb_queue"]
 
         try:
             conn = stomp.Connection([(host, port)])
             conn.start()
             conn.set_ssl(for_hosts=[(host, port)], ssl_version=ssl.PROTOCOL_TLS)
-            conn.connect(username, password, wait=True)
-            conn.send(body=message, destination=queue_name)
+            conn.connect(
+                self.config.get("plugins:stomp", "username"),
+                self.config.get("plugins:stomp", "password"),
+                wait=True,
+            )
+            conn.send(
+                body=message, destination=self.config.get("plugins:stomp", "queue")
+            )
             conn.disconnect()
             touch_file(CHECK_FILE_AGE_FILE)
         except Exception as ex:  # pylint: disable=broad-except
